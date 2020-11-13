c5193a6427595ebb582c4d28a3be0ff3566a58b3
Astrayesa <jsparrow0404@gmail.com>
2020-11-13 19:39:45 +0700

    template AdminLTE, database, model, seed

---
+++ app/Controllers/Ecom.php
@@ -0,0 +1,9 @@
+<?php namespace App\Controllers;
+
+class Ecom extends BaseController
+{
+    public function index()
+    {
+        return view('e-commerce');
+    }
+}
---
+++ app/Controllers/Projects.php
@@ -0,0 +1,9 @@
+<?php namespace App\Controllers;
+
+class Projects extends BaseController
+{
+    public function index()
+    {
+        return view('projects');
+    }
+}
